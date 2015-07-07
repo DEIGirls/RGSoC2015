a2e79f82e1bffd385c2f8a95d0b2e85ab72bb1ba
Ines <ines.opcoelho@gmail.com>
2015-07-07 15:54:54 +0100

    Update model specs

--- Diary/spec/models/entry_spec.rb
+++ Diary/spec/models/entry_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  #it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates new entry"
+  it "updates entry"
+  it "deletes entry"
+  it "shows entry"
+  it "lists entries of a given date"
+
+  it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
+  it { should validate_length_of(:description).is_at_least(1).is_at_most(255) }
+  it { should validate_length_of(:author).is_at_least(1).is_at_most(255) }
 
 end
