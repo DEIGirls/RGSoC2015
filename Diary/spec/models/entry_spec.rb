37cbae3bc21a4dded33fb5d349fb8b9365bc5aa2
Ines <ines.opcoelho@gmail.com>
2015-07-07 16:06:22 +0100

    Update model specs

--- Diary/spec/models/entry_spec.rb
+++ Diary/spec/models/entry_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  it "creates new entry"
-  it "updates entry"
-  it "deletes entry"
-  it "shows entry"
-  it "lists entries of a given date"
 
   it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
   it { should validate_length_of(:description).is_at_least(1).is_at_most(255) }
   it { should validate_length_of(:author).is_at_least(1).is_at_most(255) }
+  it { should allow_value('Ines', 'Rosa').for(:author) }
+  it { should_not allow_value('coiso', 'XD', 'GX', '', nil).for(:author) }
 
 end
