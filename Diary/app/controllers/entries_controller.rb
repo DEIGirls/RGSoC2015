dab74cec38fb834172c1359e9f64d58361b55c34
Christian Weyer <caducitynights@gmail.com>
2015-07-09 14:43:31 +0200

    Some cleanups we did during a Screenhero, wasn't me though ;)

--- Diary/app/controllers/entries_controller.rb
+++ Diary/app/controllers/entries_controller.rb
@@ -1,37 +1,53 @@
 class EntriesController < ApplicationController
+	before_action :authenticate, only: [:new, :create, :edit, :update, :destroy]
+
+private
+	def entry_params
+	  params.require(:entry).permit(:title, :description, :author)
+	end
+
+	def authenticate
+		authenticate_or_request_with_http_basic do |u,p|
+			u == 'Rosa' && p == 'banana' || u == 'Ines' && p == 'morango'
+		end
+	end
+
+public
 	def index
-		@new_entry = Entry.new
-		@all_entries = Entry.order(created_at: :desc).all
+		@entries = Entry.order(created_at: :desc).all
 	end
 
-	def add_entry
-		@new_entry = Entry.new
+	def new
+		@entry = Entry.new
 	end
 
-	def create 
-		@new_entry = Entry.new(entry_params) 
-    	@new_entry.save 
-		redirect_to '/admin'
+	def create
+		@entry = Entry.new(entry_params)
+
+		if @entry.save
+			redirect_to entries_path
+		else
+			render :new
+		end
 	end
 
-	def delete
-		@entry = Entry.find(params[:id])
-		@entry.delete
-		redirect_to '/admin'
-	end
+
 
 	def edit_entry
 		@entry = Entry.find(params[:id])
 	end
-	
+
 	def update
 		@entry = Entry.find(params[:id])
 		@entry.update(entry_params)
 		redirect_to '/admin'
 	end
-	
-	private
-	def entry_params
-	  params.require(:entry).permit(:title, :description, :author)
+
+	def destroy
+		@entry = Entry.find(params[:id])
+		@entry.delete
+		redirect_to entries_path
 	end
+
+
 end
