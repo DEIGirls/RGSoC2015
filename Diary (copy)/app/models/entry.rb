1ae14233711c8e7b8f6512cb1c70650cf7aa2033
Ines <ines.opcoelho@gmail.com>
2015-07-10 09:28:31 +0100

    Remove unused project

--- Diary (copy)/app/models/entry.rb
+++
@@ -1,8 +0,0 @@
-class Entry < ActiveRecord::Base
-	validates :title, length: {  minimum:1, maximum:255}
-	validates :description, length: {  minimum:1, maximum:4096}
-	validates :author, length: {  minimum:1, maximum:4}
-	validates :author, inclusion: { in: %w(Rosa Ines)}
-	validates :author, exclusion: { in: %w(coiso XD GX '' nil)}
-
-end
